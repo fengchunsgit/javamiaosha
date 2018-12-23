d7fb946893c3c044075e2811a1de7192b53ed956
fengchunsgit <1055688803@qq.com>
2018-12-23 22:41:54 +0800

    'springboot'

--- src/main/java/com/miaosha/App.java
+++ src/main/java/com/miaosha/App.java
@@ -1,14 +1,25 @@
 package com.miaosha;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@EnableAutoConfiguration
+@RestController
 public class App 
 {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello!";
+    }
     public static void main( String[] args )
     {
-
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
 }
